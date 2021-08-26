9e8acba0dc4217775e3d8466aab915b11ffba6ff
lfoster1150 <lfoster1150@gmail.com>
2021-08-26 11:55:52 -0500

    Added home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="App">
+      <main>
+        <section>{/* <User task={task} /> */}</section>
+      </main>
+    </div>
+  )
+}
+
+export default Home
