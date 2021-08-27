35890a00b1774f4903355aaa51eddff0ae537a6b
lfoster1150 <lfoster1150@gmail.com>
2021-08-27 12:10:39 -0500

    Changes

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const controllers = require('../controllers/ExampleController')
 const AppRouter = Router()
 
-AppRouter.get('/', (req, res) => res.send('This is root!'))
+//AppRouter.get('/', (req, res) => res.send('This is root!'))
 
 AppRouter.post('/task', controllers.createTask)
 AppRouter.get('/task/:id', controllers.getTaskById)
