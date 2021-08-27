4344f464a50978fd2083e9771b5a8a2e3604f2ba
lfoster1150 <lfoster1150@gmail.com>
2021-08-26 19:54:19 -0500

    MErging all

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
-
+import User from './pages/User'
+import { Route, Switch } from 'react-router-dom'
+import TaskList from './components/TaskList'
 
 function App() {
+  const [currentUsername, setCurrentUsername] = useState('')
   return (
     <div className="App">
       <header>
         <h1> Task Manager</h1>
       </header>
-      <Home />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={(props) => (
+            <Home
+              {...props}
+              currentUsername={currentUsername}
+              setCurrentUsername={setCurrentUsername}
+            />
+          )}
+        />
+        <Route
+          path="/users"
+          component={(props) => (
+            <User {...props} currentUsername={currentUsername} />
+          )}
+        />
+        {/* <Route
+          path="/users/:userName/taskLists/:taskListId"
+          component={TaskList}
+        /> */}
+      </Switch>
     </div>
   )
 }
